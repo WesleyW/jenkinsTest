2627f6d574525f89ce14c3e0e8a888e2c3b5086c
WesleyW <wang2587@berkeley.edu>
2018-08-07 15:52:59 -0700

    Update detectMergeConflict.py

--- detectMergeConflict.py
+++ detectMergeConflict.py
@@ -2,18 +2,16 @@ import os
 import json
 import requests
 
-# Authentication for user filing issue (must have read/write access to
-# repository to add issue to)
+#Authentication and repository information
 USERNAME = os.environ['ghUsername']
 PASSWORD = os.environ['ghPassword']
-
-# The repository to add this issue to
 REPO_OWNER = os.environ['repoOwner']
 REPO_NAME = os.environ['repoName']
 
-
 pr_url = 'https://api.github.com/repos/%s/%s/pulls/2' % (REPO_OWNER, REPO_NAME)
 
+session = requests.Session()
+session.auth = (USERNAME, PASSWORD)
 response = session.get(pr_url)
 
 if response == null:
@@ -23,26 +21,9 @@ content = json.loads(response).content
 
 if not content.mergeable:
     comment_url = 'https://api.github.com/repos/%s/%s/issues/2/comments' % (REPO_OWNER, REPO_NAME)
-
-def make_github_issue(title, body=None, labels=[]):
-    '''Create an issue on github.com using the given parameters.'''
-        
-    # Create an authenticated session to create the issue
-    session = requests.Session()
-    session.auth = (USERNAME, PASSWORD)
-    # Create our issue
-#     issue = {'title': title,
-#              'body': body,
-#              'labels': labels}
-    # Add the issue to our repository
-#     r = session.post(url, json.dumps(issue))
-    r = session.get(url)
-    content = json.loads(r.content)
-    print content['mergeable']
-#     if r.status_code == 201:
-#         print ('Successfully created Issue {0:s}'.format(title))
-#     else:
-#         print ('Could not create Issue {0:s}'.format(title))
-#         print ('Response:', r.content)
-        
-make_github_issue("TITLE")
+    comment = {"body": "Your pull request has resulted in a merge conflict error."}
+    session.post(comment_url, comment)
+    
+    label_url = 'https://api.github.com/repos/%s/%s/issues/2/labels' % (REPO_OWNER, REPO_NAME)
+    label = 'pr: don't merge - has merge conflicts'
+    session.post(label_url, label)
