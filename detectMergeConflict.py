5d7a3bbcda6e5852f76713d266e27f81e8aa76c3
WesleyW <wang2587@berkeley.edu>
2018-08-07 14:39:21 -0700

    Update detectMergeConflict.py

--- detectMergeConflict.py
+++ detectMergeConflict.py
@@ -27,7 +27,7 @@ def make_github_issue(title, body=None, labels=[]):
     # Add the issue to our repository
 #     r = session.post(url, json.dumps(issue))
     r = session.get(url)
-    content = r.content
+    content = json.loads(r.content)
     print content['mergeable']
 #     if r.status_code == 201:
 #         print ('Successfully created Issue {0:s}'.format(title))
