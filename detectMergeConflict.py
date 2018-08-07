487ca04ff70a008e29cb865334018cbc1e203a61
WesleyW <wang2587@berkeley.edu>
2018-08-07 16:19:54 -0700

    wait timer for github to calculate merge conflicts

--- detectMergeConflict.py
+++ detectMergeConflict.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import time
 
 #Authentication and repository information
 USERNAME = os.environ['ghUsername']
@@ -8,18 +9,29 @@ PASSWORD = os.environ['ghPassword']
 REPO_OWNER = os.environ['repoOwner']
 REPO_NAME = os.environ['repoName']
 
-pr_url = 'https://api.github.com/repos/%s/%s/pulls/2' % (REPO_OWNER, REPO_NAME)
-
 session = requests.Session()
 session.auth = (USERNAME, PASSWORD)
-response = session.get(pr_url)
 
-if response == None:
-    raise 'No response from GitHub.'
+#Returns whether the PR is mergeable - i.e. has no merge conflicts
+def get_pr_mergeable():
+    pr_url = 'https://api.github.com/repos/%s/%s/pulls/2' % (REPO_OWNER, REPO_NAME)
+    response = session.get(pr_url)
 
-content = json.loads(response.content)
+    if response == None:
+        raise 'No response from GitHub.'
 
-if not content['mergeable']:
+    return json.loads(response.content)['mergeable']
+
+wait_time = 0
+mergeable = get_pr_mergeable()
+while mergeable == None:
+    if wait_time > 30:
+        raise 'Github has not calculated PR merge conflicts within 30 seconds.'
+    time.sleep(5)
+    wait_time += 5
+    mergeable = get_pr_mergeable()
+
+if not mergeable:
     comment_url = 'https://api.github.com/repos/%s/%s/issues/2/comments' % (REPO_OWNER, REPO_NAME)
     comment = {"body": "Your pull request has resulted in a merge conflict error."}
     print session.post(comment_url, json=comment).content + "\n____________________________\n"
