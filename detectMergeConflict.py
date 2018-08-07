c9c8d4fa30dac030c0182b0cf7ed4be7d70873a3
WesleyW <wang2587@berkeley.edu>
2018-08-06 17:37:46 -0700

    Update detectMergeConflict.py

--- detectMergeConflict.py
+++ detectMergeConflict.py
@@ -14,20 +14,21 @@ REPO_NAME = os.environ['repoName']
 def make_github_issue(title, body=None, labels=[]):
     '''Create an issue on github.com using the given parameters.'''
     # Our url to create issues via POST
-    url = 'https://api.github.com/repos/%s/%s/issues' % (REPO_OWNER, REPO_NAME)
+    url = 'https://api.github.com/repos/%s/%s/pulls' % (REPO_OWNER, REPO_NAME)
+    
     # Create an authenticated session to create the issue
     session = requests.Session()
     session.auth = (USERNAME, PASSWORD)
     # Create our issue
-    issue = {'title': title,
-             'body': body,
-             'labels': labels}
+#     issue = {'title': title,
+#              'body': body,
+#              'labels': labels}
     # Add the issue to our repository
-    r = session.post(url, json.dumps(issue))
-    if r.status_code == 201:
-        print ('Successfully created Issue {0:s}'.format(title))
-    else:
-        print ('Could not create Issue {0:s}'.format(title))
-        print ('Response:', r.content)
+    r = session.get(url, json.dumps(issue))
+#     if r.status_code == 201:
+#         print ('Successfully created Issue {0:s}'.format(title))
+#     else:
+#         print ('Could not create Issue {0:s}'.format(title))
+#         print ('Response:', r.content)
         
 make_github_issue("TITLE")
