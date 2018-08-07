b5aa41c75cf4b9f80225d475c59820eb980b9fad
WesleyW <wang2587@berkeley.edu>
2018-08-07 16:23:47 -0700

    Update detectMergeConflict.py

--- detectMergeConflict.py
+++ detectMergeConflict.py
@@ -22,6 +22,7 @@ def get_pr_mergeable():
 
     return json.loads(response.content)['mergeable']
 
+#Will wait 30 seconds for Github to calculate merge conflicts in this PR
 wait_time = 0
 mergeable = get_pr_mergeable()
 while mergeable == None:
